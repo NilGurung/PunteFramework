dff940a53ae1875fa200a77169885eb3dde5deb5
Ankit Pokhrel <ankitpokhrel@gmail.com>
2012-12-13 21:20:34 +0545

    Just started to work

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+ define ( 'SEPARATOR', DIRECTORY_SEPARATOR );
+ define ( 'BASE_PATH', dirname(realpath(__FILE__)) ); //realpath gives absolute path without some constraints like ','
+ define ( 'APP_PATH', BASE_PATH . SEPARATOR . "application" . SEPARATOR );
+ define ( 'SYSTEM_PATH', BASE_PATH . SEPARATOR . "system" . SEPARATOR );
+ 
+ # Uncomment line below to enable error reporting 
+ # define ( 'DEVELOPMENT_MODE', 'true' );
+ 
+ function setErrorReporting(){
+     if( defined('DEVELOPMENT_MODE') && DEVELOPMENT_MODE ) {        
+         error_reporting( E_ALL );
+         ini_set( 'display_errors', 'true' );
+     } else {         
+         error_reporting( 0 ); //disable errors
+         ini_set( 'display_errors', 'false' );
+         ini_set( 'log_errors', 'on' );
+         ini_set( 'error_log', BASE_PATH . SEPARATOR . "temp" . SEPARATOR . "logs" . SEPARATOR . "errors.log" );
+     }
+ }
+ 
+ setErrorReporting();
+
+ include SYSTEM_PATH . "core" . SEPARATOR . "system.php";
---
+++ readme.md
@@ -0,0 +1,15 @@
+#Punte-Framework
+===============
+
+###A Light Weight PHP Framework.
+
+This project aims to build a light weight PHP framework using MVC architecture. Successful completion of this 
+project will result in an application where it is easier to modify either the visual appearance of the application 
+or the underlying business rules without affecting the other.
+
+###Objectives
+The final output of this project will be a light weight, open source, PHP framework for rapid web application development. The final project will also include some projects built using this framework to demonstrate the use of this framework. This framework will have following qualities:
+  1. It's going to be simple and light.
+  2. It will include some libraries especially focused to local Nepali Developers.
+  3. The application made with this framework will have clean and optimized code at end.
+  4. It may not take into consideration advanced practices or security measures for now.
